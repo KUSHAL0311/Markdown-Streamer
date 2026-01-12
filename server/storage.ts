3a081c858347b855c860cf7703bc40d6327a9100
KUSHAL0311 <kushalramanath@gmail.com>
2026-01-12 20:45:11 +0530

    Add files via upload

---
+++ server/storage.ts
@@ -0,0 +1,24 @@
+import { db } from "./db";
+import {
+  items,
+  type InsertItem,
+  type Item
+} from "@shared/schema";
+
+export interface IStorage {
+  getItems(): Promise<Item[]>;
+  createItem(item: InsertItem): Promise<Item>;
+}
+
+export class DatabaseStorage implements IStorage {
+  async getItems(): Promise<Item[]> {
+    return await db.select().from(items);
+  }
+
+  async createItem(insertItem: InsertItem): Promise<Item> {
+    const [item] = await db.insert(items).values(insertItem).returning();
+    return item;
+  }
+}
+
+export const storage = new DatabaseStorage();
